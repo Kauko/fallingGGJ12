f0c1abb337e3abb18e3200892f3b9eef52713928
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2012-01-27 23:26:31 +0200

    Starting

---
+++ Falling/Falling/Grid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Falling
+{
+    class Grid
+    {
+        int x, y, tileSize;
+        int rows, cols;
+
+
+    }
+}
