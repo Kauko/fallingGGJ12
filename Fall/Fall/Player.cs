7f08610c9a0c0c229624d2031b325ad79fd0daa5
PC_Vuokra <PC_Vuokra@PC_Vuokra-PC.panoulu.local>
2012-01-28 11:23:15 +0200

    Lisätty LevelLoader

--- Fall/Fall/Player.cs
+++ Fall/Fall/Player.cs
@@ -7,8 +7,50 @@ namespace Falling
 {
     class Player
     {
-        // TODO Pelaaja sisältää tilan
-    
-    
+        int row, col;
+        int turnsLeft;
+
+        public Player() 
+        {
+            this.row = C.startRow;
+            this.col = C.startCol;
+            this.turnsLeft = C.playerTurns;
+        }
+
+        public int getRow()
+        {
+            return row;
+        }
+
+        public int getCol()
+        {
+            return col;
+        }
+
+        public void setRow(int r)
+        {
+            this.row = r;
+        }
+
+        public void setCol(int c) 
+        {
+            this.col = c;
+        }
+
+        public void setPosition(int r, int c) 
+        {
+            setCol(c);
+            setRow(r);
+        }
+
+        public void decrementTurnsLeft() 
+        {
+            turnsLeft--;
+        }
+
+        public int getTurnsLeft() 
+        {
+            return turnsLeft;
+        }
     }
 }
